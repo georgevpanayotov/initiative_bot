9bb3ce24f8a476532bfa1d495f6a9c2f93f0ce5d
George Panayotov <georgep1245@gmail.com>
2022-12-10 15:18:46 -0800

    Add some model classes to represent rolling.

---
+++ roll_model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from enum import Enum
+
+class SecondRoll(Enum):
+    ADVANTAGE = 1
+    DISADVANTAGE = 1
+
+class Roll:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+        # For now, we are just expecting a first roll. This will change if we have to update for
+        # advantage.
+        self.secondRoll = None
+
+
+# Represents a round of rolling for initiative.
+class RollingRound:
+    def __init__(self, channelId):
+        self.context = ChannelContext.load(channelId)
+        self.rolls = {}
+        self.mommyId = None
