dd29d667c3d70e1ce3e563ee34c7ee4f5c9c3105
George Panayotov <georgep1245@gmail.com>
2022-12-10 14:44:53 -0800

    Add a ChannelContext class to keep persistent Metadata for the channel.

---
+++ channel_context.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+import json
+
+CHARACTER_KEY = "character"
+USER_ID_KEY = "user_id"
+USER_RECORDS_KEY = "user_records"
+ADMIN_KEY = "admin"
+EVERYONE_KEY = "everyone"
+
+class ChannelContext:
+    @staticmethod
+    def load(channelId):
+        try:
+            with open(ChannelContext._getConfigFile(channelId)) as config_file:
+                config = json.load(config_file)
+                return ChannelContext(channelId, config)
+        except FileNotFoundError:
+            print("Creating config.")
+            return ChannelContext(channelId, {})
+
+    def admin(self):
+        return self.config[ADMIN_KEY]
+
+    def setAdmin(self, userId):
+        self.config[ADMIN_KEY] = userId
+        self._saveConfig()
+
+    def everyoneId(self):
+        return self.config[EVERYONE_KEY]
+
+    def getUserId(self, character):
+        for userRecord in self.config[USER_RECORDS_KEY]:
+            if userRecord[CHARACTER_KEY] == character:
+                return userRecord[USER_ID]
+
+        return None
+
+
+    def getCharacter(self, userId):
+        for userRecord in self.config[USER_RECORDS_KEY]:
+            if userRecord[USER_ID] == userId:
+                return userRecord[CHARACTER_KEY]
+
+        return None
+
+    def setCharacter(self, userId, character):
+        for userRecord in self.config[USER_RECORDS_KEY]:
+            if userRecord[USER_ID] == userId:
+                userRecord[CHARACTER_KEY] = character
+                break
+        else:
+            self.config[USER_RECORDS_KEY].append({
+                USER_ID_KEY: userId,
+                CHARACTER_KEY: character})
+        self._saveConfig()
+
+    def __init__(self, channelId, config):
+        # Look up config for channel by ID (if found)
+        self.channelId = channelId
+        self.config = config
+
+    def _saveConfig(self):
+        with open(ChannelContext._getConfigFile(self.channelId), mode="w") as config_file:
+            json.dump(self.config, config_file)
+
+    @staticmethod
+    def _getConfigFile(channelId):
+        return f".config/channel_{channelId}"
