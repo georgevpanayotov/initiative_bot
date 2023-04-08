de998404aa6321891980cb6f1935e61b884cca54
George Panayotov <georgep1245@gmail.com>
2023-04-07 18:58:03 -0700

    Add handlers to the rootlogger to capture more messages.
    - Add a generic handler that captures all logging.
    - Add an error handler so all errors are in one place.

--- logging_config.py
+++ logging_config.py
@@ -2,6 +2,17 @@ import logging
 
 
 def configure():
+    rootLogger = logging.getLogger("discord")
+    rootHandler = logging.FileHandler("rootlog.txt")
+    rootHandler.setLevel(logging.INFO)
+    rootHandler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+    rootLogger.addHandler(rootHandler)
+
+    errorHandler = logging.FileHandler("errorlog.txt")
+    errorHandler.setLevel(logging.ERROR)
+    errorHandler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+    rootLogger.addHandler(errorHandler)
+
     logger = logging.getLogger("discord.initiative")
 
     handler = logging.FileHandler("log.txt")
